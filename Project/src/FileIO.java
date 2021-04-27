af42bcd3f1522d3cba89350e2d95c445a3011458
ftsouvelekakis <fotistsouv95@gmail.com>
2021-04-27 03:14:58 +0300

    Add method to parse fileDownloadList.txt

--- Project/src/FileIO.java
+++ Project/src/FileIO.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class FileIO{
     
     public static String readPeerFiles(){
@@ -12,5 +15,22 @@ public class FileIO{
         }
         fileNames = fileNames.substring(0,fileNames.length() - 1);
         return fileNames;
+    }
+
+    public static ArrayList<String> readListFile(){
+        ArrayList<String> fileList = new ArrayList<>();
+        try {
+            
+            File myObj = new File("Project/src/fileDownloadList.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                fileList.add(myReader.nextLine());
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return fileList;
     }
 }
